754d5444a0914ed605b60e9b4e3da2ff106a786d
Kiruubel <kiruukass@gmail.com>
2022-09-13 12:44:50 -0700

    C is not so fun

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,23 +3,15 @@
 #include <stdio.h>
 
 /**
-
  * print_sign - a function that prints the sign of a number
-
  * @n: number input
-
  * Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
-
  */
 
 int print_sign(int n)
-
 {
-
 	if (n > 0)
-
 	{
-
 		_putchar ('+');
 
 		return (1);
