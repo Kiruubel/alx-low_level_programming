56c1a69acda65a6e0d74aaee0ba814c298d16113
Kiruubel <kiruukass@gmail.com>
2022-09-26 09:27:57 -0700

    bonus

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,13 +1,11 @@
-#include "main.h"
-#include <stdio.h>
-/**
- * set_string - redirect.
- * @s: double pointer to address
- * @to: the original pointer
- *
- * Return: chessboard.
- */
-void set_string(char **s, char *to)
-{
-	*s = to;
+#include "main.h"                                                                                                                  
+                                                                                                                                   
+/**                                                                                                                                
+ * set_string - Sets the value of a pointer to a char.                                                                             
+ * @s: The pointer.                                                                                                                
+ * @to: The char.                                                                                                                  
+ */                                                                                                                                
+void set_string(char **s, char *to)                                                                                                
+{                                                                                                                                  
+        *s = to;                                                                                                                   
 }
