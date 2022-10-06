3fc920b6f90a69c082f618a08919d264b0d6151f
Kiruubel <kiruukass@gmail.com>
2022-10-06 11:17:00 -0700

    second task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,54 +1,51 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * string_nconcat - function that concatenates two strings.
+ * string_nconcat - function that concatenates two strings
  *
- *    * @s1: first string
- *     * @s2: second string
- *      * @n: memory size
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes of @s2 to add to @s1 to be a new string
  *
- *        * Return: a pointer to the string array of the 2 strings.
- *
- */
+ * Return: new string followed by the first @n bytes
+ *         of string 2 @s2 or NULL
+*/
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-
 {
+	unsigned int l1, l2, i, j;
+	char *str;
 
-	char *s3;
-
-	unsigned int i, len1, len2, total_len;
-
-	len1 = 0;
-	len2 = 0;
-
-		if (s1 == NULL)
+	/*treat NULL as empty string*/
+	if (s1 == NULL)
 		s1 = "";
-
-		if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
-		for (i = 0; s1[i] != '\0'; i++)
-		len1++;
 
-		for (i = 0; s2[i] != '\0'; i++)
-		len2++;
+	l1 = l2 = 0;
+	/*length of both s1 & s2 respectively*/
+	while (s1[l1] != '\0')
+		l1++;
+	while (s2[l2] != '\0')
+		l2++;
 
-		if (n >= len2)
-		n = len2;
-		total_len = len1 + n;
+	/*set n to length of s2*/
+	if (n >= l2)
+		n = l2;
 
-		s3 = malloc(sizeof(char) * (total_len + 1));
-
-		if (s3 == NULL)
-
+	str = (char *) malloc((l1 + n + 1) * sizeof(char));
+	if (str == NULL)
 		return (NULL);
 
-		for (i = 0; i < len1; i++)
-			s3[i] = s1[i];
-
-		for (i = 0; i < n; i++)
-			s3[len1 + i] = s2[i];
-			s3[len1 + i] = '\0';
-			return (s3);
-
+	/*add s1 to str*/
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	/*add s2 n bytes to str*/
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
